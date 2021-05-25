768dd4b188517a85a498c63a5c136e5c12e0e938
Eric L <s1046230@mail.yzu.edu.tw>
2021-05-25 20:13:42 +0800

    Add files via upload

---
+++ fastinstall_k8s_RPi.sh
@@ -0,0 +1,433 @@
+sudo apt-get install -y python-gevent python-gevent-websocket
+sudo apt-get install -y python-gevent
+sudo apt-get install -y python-opencv
+sudo apt-get install -y libjasper1
+sudo apt-get install -y libqtgui4
+sudo apt-get install -y libqt4-test
+sudo apt-get install -y libcblas-dev
+sudo apt-get install -y libhdf5-dev
+sudo apt-get install -y libcblas-dev
+sudo apt-get install -y libhdf5-dev
+sudo apt-get install -y libcblas-dev
+sudo apt-get install -y libhdf5-dev
+sudo apt-get install -y libhdf5-serial-dev
+sudo apt-get install -y libatlas-base-dev
+sudo apt-get install -y libcblas-dev
+sudo apt-get install -y libhdf5-dev
+sudo apt-get install -y libhdf5-serial-dev
+sudo apt-get install -y libatlas-base-dev
+sudo apt-get install -y libjasper-dev
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python-pip
+
+pip install click Flask future iso8601 itsdangerous Jinja2 MarkupSafe numpy opencv-python picamera pip PyYAML RPi.GPIO serial setuptools Werkzeug
+pip3 install click Flask future iso8601 itsdangerous Jinja2 MarkupSafe numpy opencv-python picamera pip PyYAML RPi.GPIO serial setuptools Werkzeug
+
+
+pip install backports-abc
+pip install backports.functools-lru-cache
+pip install backports.shutil-get-terminal-size
+pip install backports.ssl-match-hostname
+pip install BeautifulSoup
+pip install beautifulsoup4
+pip install blivet
+pip install boto3
+pip install botocore
+pip install Brlapi
+pip install certifi
+pip install cffi
+pip install chardet
+pip install click
+pip install clufter
+pip install configobj
+pip install configparser
+pip install configshell-fb
+pip install contextlib2
+pip install coverage
+pip install cryptography
+pip install cupshelpers
+pip install custodia
+pip install decorator
+pip install defusedxml
+pip install di
+pip install Django
+pip install dnspython
+pip install docutils
+pip install entrypoints
+pip install enum34
+pip install ethtool
+pip install filetype
+pip install firstboot
+pip install Flask
+pip install fros
+pip install funcsigs
+pip install future
+pip install futures
+pip install gevent
+pip install greenlet
+pip install gssapi
+pip install idna
+pip install importlib-metadata
+pip install importlib-resources
+pip install iniparse
+pip install initial-setup
+pip install intel-openmp
+pip install ipaclient
+pip install ipaddr
+pip install ipaddress
+pip install ipalib
+pip install ipaplatform
+pip install ipapython
+pip install ipaserver
+pip install IPy
+pip install ipykernel
+pip install ipython
+pip install ipython-genutils
+pip install isc
+pip install isodate
+pip install itsdangerous
+pip install javapackages
+pip install Jinja2
+pip install jmespath
+pip install jupyter-client
+pip install jupyter-core
+pip install jwcrypto
+pip install kdcproxy
+pip install keyring
+pip install kitchen
+pip install kmod
+pip install langtable
+pip install lxml
+pip install m2r
+pip install MarkupSafe
+pip install mercurial
+pip install mistune
+pip install mock
+pip install msrest
+pip install msrestazure
+pip install netaddr
+pip install netifaces
+pip install nodeenv
+pip install nose
+pip install ntplib
+pip install numpy
+pip install oauthlib
+pip install pandas
+pip install pathlib2
+pip install pcs
+pip install perf
+pip install pexpect
+pip install pickleshare
+pip install Pillow
+pip install pip
+pip install ply
+pip install policycoreutils-default-encoding
+pip install prompt-toolkit
+pip install ptyprocess
+pip install py
+pip install pyasn1
+pip install pyasn1-modules
+pip install pycparser
+pip install pycups
+pip install pycurl
+pip install Pygments
+pip install pygobject
+pip install pygpgme
+pip install pyinotify
+pip install PyJWT
+pip install pykickstart
+pip install pyliblzma
+pip install pyOpenSSL
+pip install pyparsing
+pip install pyparted
+pip install pysmbc
+pip install python-augeas
+pip install python-dateutil
+pip install python-ldap
+pip install python-linux-procfs
+pip install python-meh
+pip install python-nss
+pip install python-yubico
+pip install pytz
+pip install pyudev
+pip install pyusb
+pip install pyxattr
+pip install PyYAML
+pip install pyzmq
+pip install qrcode
+pip install qtconsole
+pip install QtPy
+pip install redis
+pip install requests
+pip install requests-oauthlib
+pip install rtslib-fb
+pip install ruamel.ordereddict
+pip install ruamel.yaml
+pip install ruamel.yaml.clib
+pip install s3transfer
+pip install scandir
+pip install schedutils
+pip install SecretStorage
+pip install seobject
+pip install sepolicy
+pip install setroubleshoot
+pip install setuptools
+pip install simplegeneric
+pip install singledispatch
+pip install six
+pip install slip
+pip install slip.dbus
+pip install soupsieve
+pip install SSSDConfig
+pip install stevedore
+pip install subp
+pip install subprocess32
+pip install suds
+pip install ta
+pip install targetcli-fb
+pip install torch
+pip install tornado
+pip install traitlets
+pip install typing
+pip install uamqp
+pip install urlgrabber
+pip install urllib3
+pip install urwid
+pip install virtualenv
+pip install virtualenv-clone
+pip install virtualenvwrapper
+pip install wcwidth
+pip install webencodings
+pip install Werkzeug
+pip install wheel
+pip install yum-langpacks
+pip install yum-metadata-parser
+pip install zipp
+pip install zope.event
+pip install zope.interface
+
+pip3 install backports-abc
+pip3 install backports.functools-lru-cache
+pip3 install backports.shutil-get-terminal-size
+pip3 install backports.ssl-match-hostname
+pip3 install BeautifulSoup
+pip3 install beautifulsoup4
+pip3 install blivet
+pip3 install boto3
+pip3 install botocore
+pip3 install Brlapi
+pip3 install certifi
+pip3 install cffi
+pip3 install chardet
+pip3 install click
+pip3 install clufter
+pip3 install configobj
+pip3 install configparser
+pip3 install configshell-fb
+pip3 install contextlib2
+pip3 install coverage
+pip3 install cryptography
+pip3 install cupshelpers
+pip3 install custodia
+pip3 install decorator
+pip3 install defusedxml
+pip3 install di
+pip3 install Django
+pip3 install dnspython
+pip3 install docutils
+pip3 install entrypoints
+pip3 install enum34
+pip3 install ethtool
+pip3 install filetype
+pip3 install firstboot
+pip3 install Flask
+pip3 install fros
+pip3 install funcsigs
+pip3 install future
+pip3 install futures
+pip3 install gevent
+pip3 install greenlet
+pip3 install gssapi
+pip3 install idna
+pip3 install importlib-metadata
+pip3 install importlib-resources
+pip3 install iniparse
+pip3 install initial-setup
+pip3 install intel-openmp
+pip3 install ipaclient
+pip3 install ipaddr
+pip3 install ipaddress
+pip3 install ipalib
+pip3 install ipaplatform
+pip3 install ipapython
+pip3 install ipaserver
+pip3 install IPy
+pip3 install ipykernel
+pip3 install ipython
+pip3 install ipython-genutils
+pip3 install isc
+pip3 install isodate
+pip3 install itsdangerous
+pip3 install javapackages
+pip3 install Jinja2
+pip3 install jmespath
+pip3 install jupyter-client
+pip3 install jupyter-core
+pip3 install jwcrypto
+pip3 install kdcproxy
+pip3 install keyring
+pip3 install kitchen
+pip3 install kmod
+pip3 install langtable
+pip3 install lxml
+pip3 install m2r
+pip3 install MarkupSafe
+pip3 install mercurial
+pip3 install mistune
+pip3 install mock
+pip3 install msrest
+pip3 install msrestazure
+pip3 install netaddr
+pip3 install netifaces
+pip3 install nodeenv
+pip3 install nose
+pip3 install ntplib
+pip3 install numpy
+pip3 install oauthlib
+pip3 install pandas
+pip3 install pathlib2
+pip3 install pcs
+pip3 install perf
+pip3 install pexpect
+pip3 install pickleshare
+pip3 install Pillow
+pip3 install pip3
+pip3 install ply
+pip3 install policycoreutils-default-encoding
+pip3 install prompt-toolkit
+pip3 install ptyprocess
+pip3 install py
+pip3 install pyasn1
+pip3 install pyasn1-modules
+pip3 install pycparser
+pip3 install pycups
+pip3 install pycurl
+pip3 install Pygments
+pip3 install pygobject
+pip3 install pygpgme
+pip3 install pyinotify
+pip3 install PyJWT
+pip3 install pykickstart
+pip3 install pyliblzma
+pip3 install pyOpenSSL
+pip3 install pyparsing
+pip3 install pyparted
+pip3 install pysmbc
+pip3 install python-augeas
+pip3 install python-dateutil
+pip3 install python-ldap
+pip3 install python-linux-procfs
+pip3 install python-meh
+pip3 install python-nss
+pip3 install python-yubico
+pip3 install pytz
+pip3 install pyudev
+pip3 install pyusb
+pip3 install pyxattr
+pip3 install PyYAML
+pip3 install pyzmq
+pip3 install qrcode
+pip3 install qtconsole
+pip3 install QtPy
+pip3 install redis
+pip3 install requests
+pip3 install requests-oauthlib
+pip3 install rtslib-fb
+pip3 install ruamel.ordereddict
+pip3 install ruamel.yaml
+pip3 install ruamel.yaml.clib
+pip3 install s3transfer
+pip3 install scandir
+pip3 install schedutils
+pip3 install SecretStorage
+pip3 install seobject
+pip3 install sepolicy
+pip3 install setroubleshoot
+pip3 install setuptools
+pip3 install simplegeneric
+pip3 install singledispatch
+pip3 install six
+pip3 install slip
+pip3 install slip.dbus
+pip3 install soupsieve
+pip3 install SSSDConfig
+pip3 install stevedore
+pip3 install subp
+pip3 install subprocess32
+pip3 install suds
+pip3 install ta
+pip3 install targetcli-fb
+pip3 install torch
+pip3 install tornado
+pip3 install traitlets
+pip3 install typing
+pip3 install uamqp
+pip3 install urlgrabber
+pip3 install urllib3
+pip3 install urwid
+pip3 install virtualenv
+pip3 install virtualenv-clone
+pip3 install virtualenvwrapper
+pip3 install wcwidth
+pip3 install webencodings
+pip3 install Werkzeug
+pip3 install wheel
+pip3 install yum-langpacks
+pip3 install yum-metadata-parser
+pip3 install zipp
+pip3 install zope.event
+pip3 install zope.interface
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+
+systemctl enable docker.service
+systemctl enable containerd.service
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" \ >> /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+sudo apt-get install -y linux-libc-dev golang gcc
+sudo apt-get install -y mingw-w64
+
+sudo sed -i 's/ rootwait$/ rootwait cgroup_enable=cpuset cgroup_enable=memory/g' /boot/cmdline.txt #need reboot
+
+swapoff -a
+kubeadm init
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+
+hostnamectl set-hostname raspberrypi2
